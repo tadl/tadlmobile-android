845373ba6ea00ea51c17417e5c6108124689204c
William Rockwood <wrockwood@tadl.org>
2013-06-29 11:16:53 -0400

    Changed an <a> to a <span> because there was no href or onclick.
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -6,7 +6,7 @@ var EVENTS_URL = 'https://www.tadl.org/mobile/export/events/json/all'
 var LOCATIONS_BASE = 'https://www.tadl.org/mobile/export/locations'
 var PLACEHOLDER_IMG = 'img/clocktower100.png';
 var FACEBOOK_URL = 'https://graph.facebook.com/TraverseAreaDistrictLibrary/feed?access_token=CAAFh5Quq0YMBAENgjPYY9MY0y3cdiAMvXmLl6Fq3H4LDZBBZBukKlXFwWPq0xMLa6hqDrfxfGqvFpBlIZCjFKg0rKdd37qHLsjwcBve4UeZAQymPksV7ddAeZAJOyaeZC05WqlLzrVpOUQEtjiCZArjB6NMUHjvU90qXZAGEOESKDgZDZD';
-var loadingmoreText = '<a class="loadmore"><img style="margin-right: 10px; margin-left: 10px;" src="img/ajax-loader-2.gif">LOADING...</a>';
+var loadingmoreText = '<span class="loadmore"><img style="margin-right: 10px; margin-left: 10px;" src="img/ajax-loader-2.gif">LOADING...</span>';
 var loadmoreText = '<a class="loadmore button" onclick="loadmore();">LOAD MORE RESULTS</a>';
 var psTitle = "TADL Mobile | ";
 var searchquery = {};
