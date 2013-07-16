fad1ad26b6aa69db3d4f4cb6307395d7d463ce34
William Rockwood <wrockwood@tadl.org>
2013-07-16 13:25:16 -0400

    buttonize update available link
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -33,9 +33,6 @@ $(document).ready(function() {
         login();
     }
     $('#search').click(getResults);
-   
-    
-    
 });
 
 function checkstatus() {
@@ -47,7 +44,7 @@ function checkstatus() {
             var message = data.message
             var update_link = data.update_link 
             if (message !== "up to date client") {
-                $('#status-messages').html('<a href="'+ update_link +'">' + message  + '</a>');
+                $('#status-messages').html('<a class="button" href="'+ update_link +'">update available!</a>');
             }
         });
 //    } 
